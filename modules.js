35720398ed91cf61b3aede9342aae965580c9bdc
Samuel Tabe <61163104+samueltabe@users.noreply.github.com>
2023-04-29 21:49:42 +0100

    server first commit

--- modules.js
+++
@@ -1,7 +0,0 @@
-const xyz = require('./people');
-
-console.log(xyz.ages, xyz.people)
-
-const os = require('os');
-
-console.log(os.platform(), os.homedir(), os.freemem(), os.hostname(), os.getPriority());
