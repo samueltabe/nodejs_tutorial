30d8d61b38c2f9d34c30a2481a416aae080049f8
Samuel Tabe <61163104+samueltabe@users.noreply.github.com>
2023-04-30 22:09:49 +0100

    more on server

---
+++ server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+
+
+    //set header content type
+    res.setHeader('Conteent-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+        break;
+
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+        break;
+
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+        break;
+        
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+        break; 
+
+    }
+
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err)
+            res.end();
+        }else{
+           // res.write(data);
+            res.end(data);
+        }
+    })
+
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000');
+});
